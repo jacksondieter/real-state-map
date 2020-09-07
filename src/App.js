c4b2e55a0356df91ddcd3a639bd9f5ba317782a6
Dieter Jackson <jacksondieter@gmail.com>
2020-09-07 02:34:47 +0200

    feat:Adding global state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { StateProvider } from './State';
 import MapComponent from './components/Map';
+import Sidebar from './components/Sidebar'
 
 
 function App() {
+  const initialState = {
+    geoData: []
+  };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'loadData':
+        return {
+          ...state,
+          geoData: action.newGeoData
+        };
+      case 'cleanData':
+        return {
+          ...state,
+          geoData: []
+        };
+      default:
+        return state;
+    }
+  };
   return (
-    <MapComponent />
+    <div className={'page-container'}>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <Sidebar/>
+        <MapComponent />
+      </StateProvider>
+    </div>
   );
 }
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,87 @@
+import React,{useState} from 'react'
+import {CSVReader} from 'react-papaparse'
+import { useStateValue } from '../State';
+import {_getGeoJsonArray,config} from '../utils/handler'
+
+
+const Sidebar = () => {
+    const [{ geoData }, dispatch] = useStateValue();
+    //const [geoData, setGeoData] = useState([])
+
+    const handleOnDrop = (data) => {
+        console.log('***************************')
+        console.log(data)
+        console.log('---------------------------')
+        const jsonArray = data.map(x => x.data)
+        const geoJsonArray = _getGeoJsonArray(jsonArray)
+        console.log(geoJsonArray)
+        dispatch({
+            type:'loadData',
+            newGeoData:geoJsonArray
+        })
+        //setGeoData(geoJsonArray)
+        console.log('***************************')
+    }
+
+    const handleOnError = (err, file, inputElem, reason) => {
+        console.log(err)
+    }
+
+    const handleOnRemoveFile = (data) => {
+        console.log('===========================')
+        console.log(data)
+        dispatch({
+            type:'cleanData'
+        })
+        console.log('---------------------------')
+    }
+
+    return (
+        <div className={'sidebar-container'}>
+            <div className={'bar-container'}>
+            <CSVReader
+                onDrop={handleOnDrop}
+                noDrag
+                noProgressBar
+                onError={handleOnError}
+                addRemoveButton
+                onRemoveFile={handleOnRemoveFile}
+                style={{
+                    dropArea: {
+                        borderRadius: 20,
+                        height: 50,
+                        padding:0
+                    },
+                    dropAreaActive: {
+                        borderColor: 'red',
+                    },
+                    dropFile: {
+                        width: '100%',
+                        height: 50,
+                    },
+                    fileSizeInfo: {
+                        display:'none'
+                    },
+                    fileNameInfo: {
+                        borderRadius: 3,
+                        fontSize: 14,
+                        lineHeight: 1,
+                        padding: '0 0.4em',
+                    },
+                    removeButton: {
+                        color: 'blue',
+                    },
+                }}
+                config={config}
+            >
+                        <span>Click to upload.</span>
+            </CSVReader>
+            </div>
+            <div>
+                {JSON.stringify(geoData, null, 4)}
+            </div>
+        </div>
+    )
+}
+
+export default  Sidebar
